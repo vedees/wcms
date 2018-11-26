15dca44dd40ec00b63c5dc4bbf41f8b2f4733b3f
Evgeny Vedegis <tichoo@yandex.ru>
2018-11-27 00:00:38 +0300

    Add LICENSE & Copyright

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-/**
+/*!
+ * WCMS v0.0.1 - WEX Simple CMS
+ * https://github.com/wexcms/wcms
+ * Copyright (C) 2018 Evgenii Vedegis <vedegis@gmail.com>
+ * https://github.com/wexcms/wcms/master/LICENSE
+ */
+
+ /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue & other libraries and stylus files. It is a great starting
  * point when building robust, powerful web applications using Vue and WCMS.
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * WCMS v0.0.1 - WEX Simple CMS
+ * https://github.com/wexcms/wcms
+ * Copyright (C) 2018 Evgenii Vedegis <vedegis@gmail.com>
+ * https://github.com/wexcms/wcms/master/LICENSE
+ */
