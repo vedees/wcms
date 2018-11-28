2474a17ab7acc3ed6149575b856202ae910cfc52
Evgeny Vedegis <tichoo@yandex.ru>
2018-11-28 22:51:19 +0300

    Add initialize & includes

---
+++ wcms/wex/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Server fix -->
+  <link rel="stylesheet" href="http://localhost:8080/wcms/wex/static/css/main.css">
+  <!-- Deploy settings -->
+  <!-- <link rel="stylesheet" href="/wcms/wex/static/css/main.css"> -->
+	<title><?php echo $page_title; ?></title>
+</head>
+<body>
---
+++ wcms/wex/initialize.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+require '../config.php';
+require 'function.php';
